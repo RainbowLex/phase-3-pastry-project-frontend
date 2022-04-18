170c293dece00170750a903d934f8fc8f47cec27
Sam <samuel.waters@gmail.com>
2022-04-18 12:50:59 -0600

    more

--- my-app-frontend/src/Home.js
+++ my-app-frontend/src/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Top from './Top';
+import Login from './Login';
 import './Home.css';
 
 function Home(){
     return(
-        <div>
-            <Header className='Header'/>
-            <p>Home</p>
-            <Footer className='Footer'/>
+        <div id="home" >
+            <Header id='header'/>
+            <h1>Pastry Database for Pastrie Lovers!</h1>
+            <Login id='Login' />
+            <Top id='Top'/>
+            <Footer id='footer'/>
         </div> 
     )
 }
---
+++ my-app-frontend/src/Top.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Header.css'; 
+
+function Top(){
+    return(
+        <div id="Top">
+            <p>Top Rated Pastries Area</p>
+        </div> 
+    )
+}
+
+export default Top;
